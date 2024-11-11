be27d556d9c15f8f28f945adbc2866d8dcb9deb9
ProfaMaria <138469407+ProfaMaria@users.noreply.github.com>
2024-11-11 18:54:05 -0300

    Add files via upload

---
+++ Projeto Maria/graficos/quantidadeUsuarios.js
@@ -0,0 +1,22 @@
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar'
+        }
+    ]
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuariosPorRede()
